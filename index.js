22bd757f32b81cc9446d11068504d6bb9c7737a0
Hossein <h.dvd13@gmail.com>
2023-04-01 12:44:06 -0700

    lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(records) {
+    const val = records.find(isWin);
+    if(val === undefined) {
+        return val;
+    }
+    else {
+        return val.year;
+    }
+}
+
+function isWin(element) {
+    return element.result === "W";
+}
